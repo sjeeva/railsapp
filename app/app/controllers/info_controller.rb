40fbf8cab67cc3d63dab0a0cd979dc5da36e5669
Jeeva S. Chelladhurai <sjeeva@gmail.com>
2016-10-18 21:46:21 +0530

    Update info_controller.rb

--- app/app/controllers/info_controller.rb
+++ app/app/controllers/info_controller.rb
@@ -3,12 +3,14 @@ class InfoController < ApplicationController
   $hitcount = 0
   $hostname = Socket.gethostname
   $local_ip = Socket.gethostbyname($hostname)
+  $start_time = Time.now.strftime('%Y/%m/%d %H:%M:%S')
   def index
   end
 
   def json
     $hitcount += 1
     render json: {
+                  "Start Time" => "#{$start_time}",
                   "Hostname" => "#{$hostname}",
                   "Local Address" => "#{$local_ip}",
                   "Remote Address" => "#{request.remote_ip}",
